951b9d814e763c6d236105ff9b68c2ed4d796d2d
Kevin Luu <kevinateur@hotmail.com>
2020-03-24 19:13:17 -0400

    [#20] - Add Vector3ToInt to get tiles based on coordinates

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap walkableTilemap;
+    public Vector3Int[,] tilemapsPosition;
+    BoundsInt bounds;
+    Camera camera;
+
+    private void Start()
+    {
+        walkableTilemap.CompressBounds();
+        bounds = walkableTilemap.cellBounds;
+        camera = Camera.main;
+        GetTile();
+        print(walkableTilemap);
+    }
+
+
+    private void GetTile()
+    {
+        tilemapsPosition = new Vector3Int[bounds.size.x, bounds.size.y];
+
+        for (int x = bounds.xMin, i = 0; i < (bounds.size.x); x++, i++)
+        {
+            for (int y = bounds.yMin, j = 0; j < (bounds.size.y); y++, j++)
+            {
+                if (walkableTilemap.HasTile(new Vector3Int(x, y, 0)))
+                {
+                    tilemapsPosition[i, j] = new Vector3Int(x, y, 0);
+                }
+                else
+                {
+                    tilemapsPosition[i, j] = new Vector3Int(x, y, 1);
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 world = camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridpos = walkableTilemap.WorldToCell(world);
+            print(walkableTilemap.GetTile(gridpos));
+        }
+    }
+
+
+}
