34cd26bb14a2240375399f17db4b0a36144ed708
Patrick Pin <patrickpin840@hotmail.com>
2020-03-26 06:12:12 -0400

    [#9] Basic player/sheep mechanics

---
+++ Assets/Scripts/Sweater.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sweater : MonoBehaviour
+{
+    [SerializeField] float sweaterSpeed = 20;
+    [SerializeField] float sheepSpeed = 3;
+    public GameObject nearestSheep;
+    float proximity;
+    Vector2 targetPosition;
+
+    void Update()
+    {
+        // Player sets target position and sweater moves towards it
+        targetPosition = GameObject.Find("Wolf").GetComponent<Wolf>().targetPosition;
+        this.transform.position = Vector2.MoveTowards(this.transform.position, targetPosition, sweaterSpeed * Time.deltaTime);
+
+        // Acquire "Sweater" tag once sweater reaches target position
+        if (new Vector2(this.transform.position.x, this.transform.position.y) == targetPosition)
+        {
+            this.tag = "Sweater";
+        }
+
+        // Only closest naked sheep moves to this sweater once it acquires the "Sweater" tag
+        if (GameObject.FindGameObjectsWithTag("Sheared").Length > 0 && this.tag == "Sweater")
+        {
+            GameObject[] nakedSheep = GameObject.FindGameObjectsWithTag("Sheared");
+            proximity = (nakedSheep[0].transform.position - this.transform.position).magnitude;
+            nearestSheep = nakedSheep[0];
+
+            foreach (GameObject sheep in nakedSheep)
+            {
+                if ((sheep.transform.position - this.transform.position).magnitude < proximity)
+                {
+                    proximity = (sheep.transform.position - this.transform.position).magnitude;
+                    nearestSheep = sheep;
+                }
+            }
+           
+            // Move nearest sheep to this sweater
+            nearestSheep.transform.position = Vector2.MoveTowards(nearestSheep.transform.position, this.transform.position, sheepSpeed * Time.deltaTime);
+
+            // Face nearest sheep in direction of this sweater
+            if (nearestSheep.transform.position.x < this.transform.position.x)
+            {
+                nearestSheep.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                nearestSheep.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            // Destroy sweater once nearest sheep reaches it
+            if (nearestSheep.transform.position == this.transform.position)
+            {
+                nearestSheep.tag = "Clothed";
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Cannot throw sweater beyond obstacles
+        if (collision.gameObject.layer == 8)
+        {
+            sweaterSpeed = 0;
+            this.tag = "Sweater";
+        }
+    }
+}
---
+++ Assets/Scripts/Wolf.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 3;
+    [SerializeField] GameObject sweater;
+    public Vector2 targetPosition;
+    public bool woolHeld;
+
+    void Start()
+    {
+        woolHeld = false;
+        targetPosition = new Vector2(0, 0);
+    }
+
+    void Update()
+    {
+        //Basic WASD movement
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(-Vector2.right * playerSpeed * Time.deltaTime);
+            transform.localScale = new Vector3(-2, 2, 2);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
+            transform.localScale = new Vector3(2, 2, 2);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.Translate(-Vector2.up * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.Translate(Vector2.up * playerSpeed * Time.deltaTime);
+        }        
+        
+        // Left-click to shear sheep, only succeeds when sufficiently close to it
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D mouseHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (mouseHit.collider != null)
+            {
+                if (mouseHit.collider.tag == "Unsheared")
+                {
+                    if ((transform.position - mouseHit.transform.position).magnitude < 3)
+                    {
+                        mouseHit.collider.tag = "Sheared";
+                        woolHeld = true;
+                    }
+                }
+            }
+        }
+
+        // Right-click to throw sweater to the indicated position
+        if (woolHeld)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Instantiate(sweater, this.transform.position, Quaternion.identity);
+                targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+                woolHeld = false;
+            }
+        }
+    }
+}
