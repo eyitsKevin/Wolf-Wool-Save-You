75c314796a309706665dbddbfeacb40491b5c559
Rezza <grzz25101996@gmail.com>
2020-04-16 21:01:44 -0400

    Added looping soundtrack and controls menu
    The menu can be toggled on/off by ESC
    
    The script for the controls menu is controlsMenu.cs

---
+++ Assets/Scripts/ControlMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMenu : MonoBehaviour
+{
+    public bool visible;
+
+    void Start()
+    {
+        if (GetComponent<SpriteRenderer>().enabled)
+            visible = true;
+        else
+            visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (visible)
+            {
+                visible = false;
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+            else
+            {
+                visible = true;
+                GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+    }
+}
