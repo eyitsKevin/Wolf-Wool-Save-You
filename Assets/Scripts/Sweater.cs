4cecbbc9bc393b8fb466cfdebc78e0710c2d3da5
Patrick Pin <patrickpin840@hotmail.com>
2020-04-17 23:14:32 -0400

    Golden sheep mechanics and sweater fixes

--- Assets/Scripts/Sweater.cs
+++ Assets/Scripts/Sweater.cs
@@ -12,44 +12,49 @@ public class Sweater : MonoBehaviour
 
     void Update()
     {
-        // Player sets target position and sweater moves towards it
-        targetPosition = GameObject.Find("Wolf").GetComponent<Wolf>().targetPosition;
-        this.transform.position = Vector2.MoveTowards(this.transform.position, targetPosition, sweaterSpeed * Time.deltaTime);
-
-        // Acquire "Sweater" tag once sweater reaches target position
-        if (new Vector2(this.transform.position.x, this.transform.position.y) == targetPosition)
+        if (this.tag != "Sweater")
         {
-            this.tag = "Sweater";
-        }
+            // Player sets target position and sweater moves towards it
+            targetPosition = GameObject.Find("Wolf").GetComponent<Wolf>().targetPosition;
+            this.transform.position = Vector2.MoveTowards(this.transform.position, targetPosition, sweaterSpeed * Time.deltaTime);
 
-        // Only closest naked sheep moves to this sweater once it acquires the "Sweater" tag
-        if (GameObject.FindGameObjectsWithTag("Sheared").Length > 0 && this.tag == "Sweater")
-        {
-            GameObject[] nakedSheep = GameObject.FindGameObjectsWithTag("Sheared");
-            proximity = (nakedSheep[0].transform.position - this.transform.position).magnitude;
-            nearestSheep = nakedSheep[0];
-
-            foreach (GameObject sheep in nakedSheep)
+            // Acquire "Sweater" tag once sweater reaches target position
+            if (new Vector2(this.transform.position.x, this.transform.position.y) == targetPosition)
             {
-                if ((sheep.transform.position - this.transform.position).magnitude < proximity)
-                {
-                    proximity = (sheep.transform.position - this.transform.position).magnitude;
-                    nearestSheep = sheep;
-                }
+                this.tag = "Sweater";
             }
-
-            // Move nearest sheep to this sweater
-            nearestSheepBehaviour = nearestSheep.GetComponent<SheepBehavior>();
-            nearestSheepBehaviour.SetOldPos();
-            nearestSheepBehaviour.pathingType = SheepBehavior.SheepPathingType.ToSweater;
-            nearestSheepBehaviour.sweaterPos = transform.position;
-            nearestSheepBehaviour.travelPath.Clear();
-            
-            proximity = (nearestSheep.transform.position - this.transform.position).magnitude;
-
-            if (proximity < 3) // change layer only if sheep is about to pick up the shirt
+        }
+        else
+        {
+            // Only closest naked sheep moves to this sweater once it acquires the "Sweater" tag
+            if (GameObject.FindGameObjectsWithTag("Sheared").Length > 0)
             {
-                this.gameObject.layer = 0; // can throw sweater past sheep; sheep can only collide with sweater once it lands
+                GameObject[] nakedSheep = GameObject.FindGameObjectsWithTag("Sheared");
+                proximity = (nakedSheep[0].transform.position - this.transform.position).magnitude;
+                nearestSheep = nakedSheep[0];
+
+                foreach (GameObject sheep in nakedSheep)
+                {
+                    if ((sheep.transform.position - this.transform.position).magnitude < proximity)
+                    {
+                        proximity = (sheep.transform.position - this.transform.position).magnitude;
+                        nearestSheep = sheep;
+                    }
+                }
+
+                // Move nearest sheep to this sweater
+                nearestSheepBehaviour = nearestSheep.GetComponent<SheepBehavior>();
+                nearestSheepBehaviour.SetOldPos();
+                nearestSheepBehaviour.pathingType = SheepBehavior.SheepPathingType.ToSweater;
+                nearestSheepBehaviour.sweaterPos = transform.position;
+                nearestSheepBehaviour.travelPath.Clear();
+
+                proximity = (nearestSheep.transform.position - this.transform.position).magnitude;
+
+                if (proximity < 3) // change layer only if sheep is about to pick up the shirt
+                {
+                    this.gameObject.layer = 0; // can throw sweater past sheep; sheep can only collide with sweater once it lands
+                }
             }
         }
     }
